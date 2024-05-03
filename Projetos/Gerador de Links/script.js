b23f6a0d83daa5357db90a3bb5fc0913eb2ed9e8
natimathias <130069163+natimathias@users.noreply.github.com>
2024-05-03 09:58:13 -0300

    Gerador

---
+++ Projetos/Gerador de Links/script.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("[input]")
+const btnLink = document.querySelector("[Gerar-Link]")
+const btnSend = document.querySelector("[Copiar-Link]")
+
+function handlebtnLink(){
+    console.log("Olá Mundo")
+}
+
+btnLink.addEventListener("click", handlebtnLink)
